f7af16de415074b23da639cb13dbf943d3c2050d
Usune <101433956+Usune@users.noreply.github.com>
2023-07-14 00:51:54 -0500

    Commit Inicial

---
+++ js/Main.js
@@ -0,0 +1,7 @@
+function suma() {
+    let a = 0, b = 0, suma= 0;
+    a = parseInt(prompt("Ingerse el primer valor"));
+    b = parseInt(prompt("Ingerse el segundo valor"));
+    suma = a + b;
+    alert("La suma es igual a: " + suma)
+}
